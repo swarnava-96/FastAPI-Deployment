e7ca17bad4fe6338df65d14c6bd75a4492c98827
Swarnava Mukherjee <75041273+swarnava-96@users.noreply.github.com>
2021-06-26 19:58:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 26 19:33:53 2021
+
+@author: SWARNAVA
+"""
+# 1. Library imports
+import uvicorn ##ASGI
+from fastapi import FastAPI
+
+# 2. Create the app object
+app=FastAPI()
+
+# 3. Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message':'Hello World!'}
+
+#4. Route with a single parameter, returns the parameter within a message
+#    Located at: http://127.0.0.1:8000/AnyNameHere
+@app.get('/welcome')
+def get_name(name:str):
+    return {'Welcome to the world of Python!':f'(name'}
+
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if  __name__=='__main__':
+    uvicorn.run(app,host='127.0.0.1', port=8000)
